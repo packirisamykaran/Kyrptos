835e90d735a96971ab0fbe2fe83acbba45139103
Karan <packirisamykaran@gmail.com>
2023-09-22 10:29:11 +0800

    constant.ts

---
+++ src/requests/handlers/whale/constants.ts
@@ -0,0 +1,11 @@
+export const solscanAPI = {
+  tokenHolder: "https://public-api.solscan.io/token/holders?",
+  accountTokens: "https://public-api.solscan.io/account/tokens?",
+};
+
+export const defiAddress = {
+  Solend: "SLNDpmoWTVADgEdndyvWzroNL7zSi1dF9PC3xHGtPwp",
+  Marinade: "mSoLzYCxHdYgdzU16g5QSh3i5K3z3KZK7ytfqcJm7So",
+  Lido: "7dHbWXmci3dT8UFYWYZweBLXgycu7Y3iL6trKn1Y7ARj",
+  Orca: "orcaEKTdK7LKz57vaAYr9QeNsVEPfiu6QeMU1kektZE",
+};
