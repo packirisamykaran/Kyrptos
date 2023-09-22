fcf97634676cbf690e67e3098ac0a994d4a1812a
Karan <packirisamykaran@gmail.com>
2023-09-22 10:52:13 +0800

    nodemon

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,14 @@
 // src/app.ts
 import express from "express";
+import { defiAddress } from "./requests/handlers/whale/constants";
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+let listing = [1, 2, 4, 5, 23, 3, 324, 432523];
+
+let newlisting = listing.map((num, index) => num * 2);
+console.log(newlisting);
 
 app.get("/", (req, res) => {
   res.send("Hello, Express with TypeScript!");
